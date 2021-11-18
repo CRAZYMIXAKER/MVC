b15f8f5bd01c0a59c13fe8d97f9f2a05a9af53af
CRAZYMIXAKER <zombisuki@gmail.com>
2021-11-19 01:19:05 +0300

    article/css

--- views/article/v_article.php
+++ views/article/v_article.php
@@ -1,13 +1,20 @@
-<div>
-	<div class="">Статья</div>
-	<strong><?= $article['title'] ?></strong>
+<div class="page">
+	<div class="page__title">Статья</div>
 
-	<em><?= $article['dt_add'] ?></em>
-	<div><a href="<?= BASE_URL ?>category/categoryOne/<?= $article['id_category'] ?>"> <?= $article['name_category'] ?> </a></div>
-	<div> <?= $article['content'] ?> </div></br>
-	<?php if ($user['id_user'] === $article['id_user'] || $user['id_user'] === '1') : ?>
-		<a href="<?= BASE_URL ?>article/delete/<?= $article['id_article'] ?>"> Delete </a></br>
-		<a href="<?= BASE_URL ?>article/edit/<?= $article['id_article'] ?>"> Edit </a>
+	<div class="article">
+		<div class="article__title">
+			<div class="article__name"> <?= $article['title'] ?> </div>
+			<em class="article__date"><?= $article['dt_add'] ?></em>
+		</div>
+		<div class="article__category">
+			<a class="article__link" href="<?= BASE_URL ?>category/categoryOne/<?= $article['id_category'] ?>"> <?= $article['name_category'] ?> </a>
+		</div>
+		<div class="article__content"> <?= $article['content'] ?> </div>
+	</div>
+	<?php if ($user['id_user'] === $article['id_user'] || $user['id_access'] === '1') : ?>
+		<div class="menu">
+			<a class="menu__item" href="<?= BASE_URL ?>article/delete/<?= $article['id_article'] ?>"> Delete </a></br>
+			<a class="menu__item" href="<?= BASE_URL ?>article/edit/<?= $article['id_article'] ?>"> Edit </a>
+		</div>
 	<? endif; ?>
-	<hr>
 </div>
