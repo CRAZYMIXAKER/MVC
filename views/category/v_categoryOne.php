71774d1555fc60354e169ee40aa8c861fc66b08d
CRAZYMIXAKER <zombisuki@gmail.com>
2021-11-19 01:27:12 +0300

    categoryOne/css

--- views/category/v_categoryOne.php
+++ views/category/v_categoryOne.php
@@ -1,11 +1,13 @@
-<H1><?= $articles[0]['name_category'] ?></H1>
-<a href="<?= BASE_URL ?>">Index</a></br></br>
-<?php foreach ($articles as $article) : ?>
-	<div>
-		<strong><?= $article['title'] ?></strong>
-		<em><?= $article['dt_add'] ?></em>
-		<div> <?= $article['content'] ?> </div>
-		<a href="<?= BASE_URL ?>article/article/<?= $article['id_article'] ?>"> Read more </a>
-		<hr>
-	</div>
-<? endforeach; ?>
+<div class="page">
+	<div class="page__title">Статьи из категории '<?= $articles[0]['name_category'] ?>'</div>
+
+	<? foreach ($articles as $article) : ?>
+		<div class="article">
+			<div class="article__title">
+				<a class="article__name" href="<?= BASE_URL ?>article/article/<?= $article['id_article'] ?>"> <?= $article['title'] ?> </a>
+				<em class="article__date"><?= $article['dt_add'] ?></em>
+			</div>
+			<div class="article__content"> <?= $article['content'] ?> </div>
+		</div>
+	<? endforeach; ?>
+</div>
